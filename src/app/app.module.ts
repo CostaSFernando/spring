9eae6e250f780be673df1be98f9e1d615a68273d
Fernando Costa <costasfernando199@gmail.com>
2023-01-11 14:09:01 -0300

    create base home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardProductsComponent } from './components/shared/card-products/card-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CarouselComponent,
+    CardProductsComponent
   ],
   imports: [
     BrowserModule,
