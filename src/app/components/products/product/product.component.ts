304d8695a2963e3ef076671c0044a6f359ed4f64
Fernando Costa <costasfernando199@gmail.com>
2023-02-12 23:38:13 -0300

    increment translate on modal and view product

--- src/app/components/products/product/product.component.ts
+++ src/app/components/products/product/product.component.ts
@@ -1,3 +1,4 @@
+import { TranslationService } from 'src/app/services/translation.service';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
@@ -11,20 +12,31 @@ const products = require('../../../../assets/data/products.json')
 export class ProductComponent implements OnInit, AfterViewChecked {
 
   product: any;
+  translate: string = 'BR'
 
   constructor(
-    private activatedRouteSnapshot: ActivatedRoute
+    private activatedRouteSnapshot: ActivatedRoute,
+    private readonly translationService: TranslationService
   ) {
     const id = this.activatedRouteSnapshot.snapshot.params['id'];
     this.product = products.products.find((prd: any) => `${prd.id}` === `${id}`)
   }
 
   ngOnInit(): void {
+    this.translationService.getSelectedTranslate().subscribe(
+      res => {
+        this.translate = res.type
+      }
+    )
   }
 
   ngAfterViewChecked(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
+  }
+
+  get getDescription() {
+    return this.product.description[this.translate]? this.product.description[this.translate] : this.product.description['BR']
   }
 
 }
--- src/app/components/shared/card-products/card-products.component.ts
+++ src/app/components/shared/card-products/card-products.component.ts
@@ -1,5 +1,7 @@
+import { TranslationService } from 'src/app/services/translation.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Itranslate } from 'src/app/interfaces/translate.interface';
 
 @Component({
   selector: 'app-card-products',
@@ -10,12 +12,22 @@ export class CardProductsComponent implements OnInit {
 
   @Input() product: any;
   modal: boolean = false
+  private translate: string = 'BR';
 
   constructor(
-    private router: Router
+    private router: Router,
+    private translationService: TranslationService
   ) { }
 
+
   ngOnInit(): void {
+    this.translationService.getSelectedTranslate().subscribe(
+      resp => this.translate = resp.type
+    );
+  }
+
+  get getDescription() {
+    return this.product.description[this.translate]
   }
 
   controlModal(status: boolean) {
