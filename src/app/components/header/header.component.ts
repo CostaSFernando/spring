668161bd77d99acc4f9f578a9aeb18022e4d7972
Fernando Costa <costasfernando199@gmail.com>
2023-03-27 06:02:51 -0300

    create menu mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Itranslate } from './../../interfaces/translate.interface';
 import { Component, OnInit } from '@angular/core';
 import { TranslationService } from 'src/app/services/translation.service';
+import { Router } from '@angular/router';
 const configData = require('../../../assets/data/data.json');
 @Component({
   selector: 'app-header',
@@ -12,9 +13,11 @@ export class HeaderComponent implements OnInit {
   translations!: Itranslate[];
   translateItem!: Itranslate;
   config = configData;
+  openMenu = false;
 
   constructor(
-    private readonly translationService: TranslationService
+    private readonly translationService: TranslationService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -41,6 +44,11 @@ export class HeaderComponent implements OnInit {
 
   selectedTranslate(item: Itranslate) {
     return item.type === this.translateItem.type
+  }
+
+  redirectButton(button: string) {
+    this.openMenu = !this.openMenu;
+    this.router.navigate([button]);
   }
 
 }
